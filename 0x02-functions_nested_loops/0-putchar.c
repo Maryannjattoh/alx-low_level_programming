d03f28f782025ae944d152117171804a9e01aeae
Maryannjattoh <maryannjattoh@gmail.com>
2022-12-14 18:02:16 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - prints a message from main .h
+*
+* Return: nothing 0
+*/
+
+int main(void)
+{
+char *sh = "_putchar";
+
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+_putchar('\n');
+return (0);
+}
